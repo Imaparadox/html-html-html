5670e80eaa63488c5f350468ad7b816021ba452a
Stephen Marsh <stevemarsh117@gmail.com>
2022-01-14 20:29:04 -0500

    Hello World

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
-const express = require('express');
+const express = require('express')
+const app = express()
 
-const 
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.listen(3000)
